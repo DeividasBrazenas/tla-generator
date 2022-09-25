d6761d3f9d84c85f62b9c86824bb68746fadfb3f
DeividasBrazenas <deividas.brazenas@trafi.com>
2022-09-25 18:10:22 +0300

    Adding pluscal generator

--- lib/pluscal_generator.ex
+++ lib/pluscal_generator.ex
@@ -1,6 +1,6 @@
-defmodule Tla.Generator do
-  @spec generate(String.t(), String.t(), atom(), List[atom()]) :: List[String.t()]
-  def generate(module_name, input_file_path, generation_type, generation_defs) do
+defmodule Tla.PlusCalGenerator do
+  @spec generate(String.t(), String.t(), List[atom()]) :: List[String.t()]
+  def generate(module_name, input_file_path, generation_defs) do
     {_, ast} =
       input_file_path
       |> File.read!()
@@ -8,7 +8,7 @@ defmodule Tla.Generator do
 
     tla =
       [getHeader(module_name)] ++
-        Tla.Generator.TlaBody.get(generation_type, generation_defs, ast) ++
+        Tla.Generator.PlusCalBody.get(module_name, generation_defs, ast) ++
         [getFooter(module_name)]
 
     Enum.join(tla, "\n")
